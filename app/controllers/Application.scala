cd36bbedcb0cfc50d066d5a8b1242b724709d609
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-22 19:44:15 +0200

    First running demo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,35 +1,34 @@
 package controllers
 
-import java.util.UUID.randomUUID
+
 import play.api.mvc._
 import postranker.PostRankingService
+import postranker.domain.Post
+import scala.collection.JavaConversions._
 
 object Application extends Controller {
-
+  lazy val posts: List[Post] = rankingService.getPosts.toList
 
   def index = Action { request =>
-    val posts: List[Long] = List(1,2,3,4,5,6)
     Ok(views.html.index(posts))
   }
 
 
-  def post(id: Long) = Action { request =>
-    val postDesc =
-      """
-        | blablah blah ad sljhd jsahd kjahdkj sahkjd sjkd
-        | asjh kjAHS KJADHSKJ AHSDKJ AHSKJD AKSJHD KJASHDJK S
-        | ASJD KJASHDKJ AHSK JDAHSKJDHA SKJHD KJASHD KJSAHDKJ ASDK
-        | JASD ASJHD AHSD AGHSDG JHDGAJSGD JAHSG DJHSAG DHJGASJHD
-        | BITTI....
-      """.stripMargin
+  def post(id: String) = Action { request =>
+    posts.find(_.getSid == id).map { post =>
 
+      Ok(views.html.post(post))
 
-    Ok(views.html.post(postDesc, id))
+    }.getOrElse {
+
+      NotFound
+    }
   }
 
 
-
-  val rankingService:PostRankingService = new PostRankingService()
+  val token = "CAAE4r0cMaAoBAHqj61PYpuTZBLUBKoMsYUucwgZAkQTYqzZCMnx1YTEkVLlkbIbZBX9Nxd3lZB2RJVFUlWrF25tFQhecg1mtOZCyb345MfHYaOcpwK9r03Sletc0IRL3XQAP1OTqToUOb0ZAKvRxA2a0OxqkyCNegwzuZBLgexc1Gbf7arNCh9hwrqrTpGos5GYfZCFZBZAXikZC3YZAUoF6RHwtwUFSnZBRG63ZBUZD"
+  val rankingService: PostRankingService = new PostRankingService()
+  rankingService.init(token)
 
 
 }
