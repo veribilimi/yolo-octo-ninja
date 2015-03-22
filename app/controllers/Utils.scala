439fc54dd977aeeebf6a306e985cc936a393c439
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-23 01:01:35 +0200

    english pretty time locale

--- app/controllers/Utils.scala
+++ app/controllers/Utils.scala
@@ -6,5 +6,6 @@ import org.ocpsoft.prettytime.PrettyTime;
  * Created by sumnulu on 23/03/15.
  */
 object Utils {
-  lazy val prettyTime =  new PrettyTime(new Locale("tr"))
+//  lazy val prettyTime =  new PrettyTime(new Locale("tr"))
+  lazy val prettyTime =  new PrettyTime
 }
