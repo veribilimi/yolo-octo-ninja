82344bef2b995fd2ea8e47d699cbbe03cd0d1c3d
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-25 01:53:43 +0200

    pagesize = 40

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -19,7 +19,7 @@ object Application extends Controller {
   lazy val postsActor = PostsActor()
   lazy val userActor = UserActor()
 
-  val NumberOfItemsForEachPage = 100
+  val NumberOfItemsForEachPage = 40
 
   def index(page: Int = 1) = posts("rank", page)
 
